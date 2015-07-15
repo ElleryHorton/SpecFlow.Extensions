c53f06a9724e04648431be6e3c18b784854c7d7d
specflowC <specflowC@gmail.com>
2015-07-15 15:47:50 -0400

    workaround to mark incomplete scenarios as inconclusive

---
+++ Hooks/EmptyScenarioIsInconclusive.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Hap.Cloud.Tests.SpecFlow.Web.Hooks
+{
+    [Binding]
+    public sealed class EmptyScenarioIsInconclusive
+    {
+        private bool isEmpty = true;
+
+        [BeforeStep]
+        public void BeforeStep()
+        {
+            isEmpty = false;
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            if (isEmpty)
+                Assert.Inconclusive("The scenario is empty");
+        }
+    }
+}
