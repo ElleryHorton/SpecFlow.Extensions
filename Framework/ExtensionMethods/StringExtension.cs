46eb27b9237039b93a94761b2ab0f14074d52c35
specflowC <specflowC@gmail.com>
2015-07-22 11:06:11 -0400

    StringExtension to create unique test data
    Sometimes it is helpful to be able to create unique test data that can
    be traced back to a particular tester, a group of tests, or time.

---
+++ Framework/ExtensionMethods/StringExtension.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SpecFlow.Extensions.Framework.ExtensionMethods
+{
+    public static class StringExtension
+    {
+        private static Random _random = new Random();
+
+        public static string TestHash {get; set;}
+
+        public static string TesterHash {get; set;}
+
+        public static string Randomize(this string original, int hashLength = 4, string hashDelimit = "-")
+        {
+            if (string.IsNullOrEmpty(original))
+            {
+                return original.AppendHash();
+            }
+            else if (original.HasHash())
+            {
+                return original.RandomizeHash();
+            }
+            else
+            {
+                return original.AppendHash();
+            }
+        }
+
+        private static string AppendHash(this string original, int hashLength = 4, string hashDelimit = "-")
+        {
+            string prefix = string.Empty;
+            if (!string.IsNullOrEmpty(TesterHash))
+            {
+                prefix = string.Format("{0}{1}{2}", prefix, TesterHash, hashDelimit);
+            }
+            if (!string.IsNullOrEmpty(TestHash))
+            {
+                prefix = string.Format("{0}{1}{2}", prefix, TestHash, hashDelimit);
+            }
+
+            return string.Format("{1}{2}{0}{3}{0}{4}", hashDelimit, prefix, original, DateTime.Now.ToString("yyyyMMddHHmmssfff"), GenerateRandomHash());
+        }
+
+        private static string RandomizeHash(this string hashString, int hashLength = 4, string hashDelimit = "-")
+        {
+            return hashString.Substring(0, (hashString.Length - (hashString.Length - hashString.LastIndexOf(hashDelimit)) + 1)) + GenerateRandomHash();
+        }
+
+        public static string GenerateRandomHash(int hashLength = 4)
+        {
+            StringBuilder sb = new StringBuilder();
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            for (int i = 0; i < hashLength; i++)
+            {
+                sb.Append(chars[_random.Next(chars.Length)]);
+            }
+            return sb.ToString();
+        }
+
+        private static bool HasHash(this string hashString, int hashLength = 4, string hashDelimit = "-")
+        {
+            return hashString.Contains(hashDelimit) && (hashString.LastIndexOf(hashDelimit) + hashDelimit.Length + hashLength == hashString.Length);
+        }
+    }
+}
---
+++ Framework/ExtensionMethods/StringExtensionTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SpecFlow.Extensions.Framework.ExtensionMethods
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [SetUp]
+        public void ResetStringExtensionHashes()
+        {
+            StringExtension.TesterHash = string.Empty;
+            StringExtension.TestHash = string.Empty;
+        }
+
+
+        [Test]
+        public void StringExtensionCreatesHash()
+        {
+            Assert.AreEqual(2, StringExtension.GenerateRandomHash(2).Length);
+            Assert.AreEqual(4, StringExtension.GenerateRandomHash(4).Length);
+            Assert.AreEqual(6, StringExtension.GenerateRandomHash(6).Length);
+        }
+
+        [Test]
+        public void StringExtensionAppendsHash()
+        {
+            string temp = "temp";
+
+            Assert.IsTrue(temp.Randomize().Contains(DateTime.Now.ToString("-yyyyMMddHHmmss")));
+        }
+
+        [Test]
+        public void StringExtensionUpdatesHash()
+        {
+            List<string> hashes = new List<string>();
+
+            for (int i = 0; i < 500; i++)
+            {
+                string temp = "temp";
+                string temp1 = temp.Randomize();
+                string temp2 = temp1.Randomize();
+
+                hashes.Add(temp1);
+                hashes.Add(temp2);
+
+                Assert.AreEqual(temp1.Substring(0, 23), temp2.Substring(0, 23));
+                Assert.AreNotEqual(temp1.Substring(23, 4), temp2.Substring(23, 4), string.Format("temp1:{0} temp2:{1}", temp1, temp2));
+            }
+
+            Assert.AreEqual(hashes.Count, hashes.Distinct().Count());
+        }
+
+        [Test]
+        public void StringExtensionSetsHash()
+        {
+            string tester = "ELH";
+            string test = "SESH";
+            string temp = "temp";
+
+            StringExtension.TestHash = test;
+
+            Assert.IsTrue(temp.Randomize().StartsWith(test + "-temp"));
+
+            StringExtension.TesterHash = tester;
+
+            Assert.IsTrue(temp.Randomize().StartsWith(tester + "-" + test + "-temp"));
+
+            StringExtension.TestHash = string.Empty;
+
+            Assert.IsTrue(temp.Randomize().StartsWith(tester + "-temp-"));
+            
+
+                        
+        }
+    }
+}
